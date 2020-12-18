a55b056b88dde1e008adf170f07bf1e998198bc5
91884227 <chuck30621@gmail.com>
2020-12-18 16:44:02 +0800

    2020.12.18

---
+++ emotional_score.py
@@ -0,0 +1,13 @@
+import requests
+
+class emotional_score:
+    def __init__(self):
+        self.url = "http://140.118.155.170:5000"
+    
+    def __call__(self, str_):
+        assert type(str_)==str, "input 要是 str"
+        query = {'document': str_}
+        response = requests.post(self.url, json = query).json()
+        score = float(response['score'])
+        return(score)
+                
