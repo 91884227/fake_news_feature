2a93a2c8c6f9843311340fc1327c5dff7b3eaff3
91884227 <chuck30621@gmail.com>
2021-01-13 15:21:26 +0800

    2020.01.13

---
+++ SVM農場模型/tranfer_to_numeric_farm.py
@@ -0,0 +1,70 @@
+# cd ./fake_news_feature/SVM農場模型
+# python tranfer_to_numeric_farm.py --d Size_5000
+
+# import tool
+import os
+from tqdm import tqdm
+
+import json
+import copy
+from SVM_feature_engineering import feature_class
+
+# import argparse
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--d", default = "Size_100", type=str)
+parser.add_argument("--B", default = False, type=bool)
+parser.add_argument("--test", default = False, type=bool)
+args = parser.parse_args()
+
+
+def buf_func(file_):
+    data_path = "./SVM判別器農場資料/%s/" % args.d
+    with open(data_path + file_) as json_file:
+        data = json.load(json_file)
+
+    if( args.test):
+        data = data[:2]  
+    
+    print("Start to deal with %s" % file_)
+    data_title = [i[0] for i in data]
+    data_numeric_title = [feature_func(i) for i in tqdm(data_title)]
+
+    data_body = [i[1] for i in data]
+    data_numeric_body = [feature_func(i) for i in tqdm(data_body)]
+    
+    # new add 
+    data_numeric = [ [ float(j) for j in i] 
+                    for i in data_numeric_title + data_numeric_body]
+
+    path = "./SVM_數值資料/%s/%s" % (args.d, file_)
+    with open(path, 'w') as outfile:
+        json.dump(data_numeric, outfile)
+    
+    print("success to save file in  %s" % path )
+
+def create_index_dict():
+    a, b = copy.deepcopy(feature_func.index_dict), copy.deepcopy(feature_func.index_dict)
+    for i, v in enumerate(a):
+        a[v] = "<title:> %s" % a[v]
+
+    for i, v in enumerate(b):
+        a[i+len(b)] = "<body:> %s" % b[v]
+
+    path = "./SVM_數值資料/%s/%s" % (args.d, "index_dict.json")
+    with open(path, 'w') as outfile:
+        json.dump(a, outfile)
+
+def main():
+    os.mkdir("./SVM_數值資料/%s" % args.d)
+    buf_func("X_train.json")
+    buf_func("X_test.json")
+    create_index_dict()
+
+if __name__ == "__main__":
+    path = "./SVM判別器農場資料/%s" % args.d
+    assert os.path.exists(path), "%s不存在" % path
+    
+    feature_func = feature_class(ifBOW = args.B)
+    main()
+    
